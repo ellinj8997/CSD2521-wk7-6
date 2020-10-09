7dfb336ced403e02c6ba75bcc399bcdd227f1c0e
ellinj8997 <70241936+ellinj8997@users.noreply.github.com>
2020-10-09 09:29:53 -0400

    Update HelloTest1.java
    I added what the program does in lines 1-3, I updated lines 7-8 with my name, date and class name. I updated the comment on line 15.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,18 @@
 /*
+This program calls the main string function and prints out the message "Welcome" "Test 3"
+*/
+
+/*
  * Created on 8/3/18 by AJT
+    Updated one 10/9/2020 by Josh Ellinger
+     Java Programming I
  */
+
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
